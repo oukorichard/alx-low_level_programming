bcb0ca9eb55b44e9a69e7a1c59b25359bf98f839
oukorichad <oukorichard001@gmail.com>
2022-07-06 01:22:47 +0300

    dsvsadgx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet in this programm prints alphabets in lower case
+ * print_alphabet - Prints the alphabet in lowercase.
  *
- *
- *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
