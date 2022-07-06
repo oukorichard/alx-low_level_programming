128a0db64b01a63a1e6a3dc889325a4f66fb11d5
oukorichad <oukorichard001@gmail.com>
2022-07-06 13:12:52 +0300

    New commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,14 @@
  */
 int main (void)
 {
-	_putchar('_');
+	{
+ 	_putchar('_');
 	_putchar('p');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	}
 	_putchar ('\n');
 }
