52f706510c6bd33ca1b57338b4352d8f4648c089
oukorichad <oukorichard001@gmail.com>
2022-07-06 18:36:52 +0300

    Eddited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,16 @@
 int main (void)
 {
 	{
- 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
 	}
-	_putchar(' ');
-	return (0);
+		_putchar(' ');
+		return (0);
 
 }
