05ca94b8a56f928a944285d96392989c1691e37e
oukorichad <oukorichard001@gmail.com>
2022-07-08 11:36:38 +0300

    Another trial

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,16 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> 13c3ccadf4c9243e42f18c6dbb41adc377a4df03
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 96)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-  }
-}
+ int isuper(int c)
+ {
+   int b;
+   if(c >= 65 && c<=96)
+   {
+   return (1);
+   }
+   else
+   {
+   return (0);
+   }
+ }
