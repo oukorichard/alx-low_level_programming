df6fd710f8037d9a708eb90f4408229a46ae622c
oukorichad <oukorichard001@gmail.com>
2022-07-08 11:43:08 +0300

    Added main function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,6 +9,17 @@
  *
  * Return: Always 0.
  */
+ int int _isuper(int c); 
+ int main(void)
+ {
+     char c;
+
+     c = 'A';
+     printf("%c: %d\n", c, _isupper(c));
+     c = 'a';
+     printf("%c: %d\n", c, _isupper(c));
+     return (0);
+ }
 
  int _isuper(int c)
  {
