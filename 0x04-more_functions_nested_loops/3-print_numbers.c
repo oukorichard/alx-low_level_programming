a84fc16cf9202bca10556d0388633adf6ce8e9e0
oukorichad <oukorichard001@gmail.com>
2022-07-08 15:55:30 +0300

    Final attempt

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int e;
+
+	for (e = 48; ch < 58; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
+}
