5383931f123ef61161644255a13a9034de66f3c8
oukorichad <oukorichard001@gmail.com>
2022-07-08 12:05:24 +0300

    new updata

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,22 +5,8 @@
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  *
- * main - check the code.
- *
- * Return: Always 0.
  */
- int int _isuper(int c); 
- int main(void)
- {
-     char c;
-
-     c = 'A';
-     printf("%c: %d\n", c, _isupper(c));
-     c = 'a';
-     printf("%c: %d\n", c, _isupper(c));
-     return (0);
- }
-
+ 
  int _isuper(int c)
  {
    int b;
