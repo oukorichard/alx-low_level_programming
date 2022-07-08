37fa83ffe7b12bbd2cd2b13f673b28dc24aff8b6
oukorichad <oukorichard001@gmail.com>
2022-07-08 11:41:15 +0300

    Added comments

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,10 @@
  * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
+ *
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
 
  int _isuper(int c)
