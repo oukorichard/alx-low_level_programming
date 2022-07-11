51ecec970d6ba4f81a710e251974c2667364b7c6
oukorichad <oukorichard001@gmail.com>
2022-07-11 23:12:28 +0300

    Initial commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*swap_int - swap two integers
+*@a: is the ffirst integer
+*@b: is the second integer
+*Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+int new = *a;
+*a = *b;
+*b = new;
+}
