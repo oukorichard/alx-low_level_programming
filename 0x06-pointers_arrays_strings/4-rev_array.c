483f056414e9a9cdfad7019b95b5fd9bd1a341cd
oukorichad <oukorichard001@gmail.com>
2022-07-14 21:50:49 +0300

    First attemp

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of intergers
+ * @a : pointer to int parameters
+ * @n : pointer to int parameters
+ * Return: no
+ */
+
+void reverse_array(int *a, int n)
+{
+	int a;
+	int b;
+	int tmp;
+
+	a = 0;
+	b = n - 1;
+
+	while (i < j)
+	{
+		tmp = a[a];
+		a[a] = a[b];
+		a[a] = tmp;
+		a++;
+		b--;
+	}
+}
