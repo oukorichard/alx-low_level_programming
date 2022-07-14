3b6fbf074b04b5650279c4ce81d9a0ccee77ce4d
oukorichad <oukorichard001@gmail.com>
2022-07-14 21:25:11 +0300

    New commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,20 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+	int index = 0, dest_len = 0;
 
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
