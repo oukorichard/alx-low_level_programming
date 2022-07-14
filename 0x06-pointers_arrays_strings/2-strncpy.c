362f91262c81f06662361dec514ffea28aee2b5b
oukorichad <oukorichard001@gmail.com>
2022-07-14 21:42:51 +0300

    Declearing new variables (int a)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int ;
+	int a;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	for ( ; a < n; a++)
+		dest[a] = '\0';
 
 	return (dest);
 }
