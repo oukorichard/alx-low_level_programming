014f3d76bbc8818e35e55dfef0ffaa0d3391b445
oukorichad <oukorichard001@gmail.com>
2022-07-21 20:25:30 +0300

    Correcting the code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
