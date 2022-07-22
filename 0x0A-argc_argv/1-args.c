91813b76481d78cd8c15003aac451ee300da0d44
oukorichard <oukorichard001@gmail.com>
2022-07-22 15:34:37 +0300

    new commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: 0 - success.
  */
 
-int main(int argc, char  *argv[]) {
-
-  printf("%d\n", argc--);
+int main(int argc, char  *argv[])
+{
+  printf("%d\n", argc - 1);
 
   return (0);
 }
