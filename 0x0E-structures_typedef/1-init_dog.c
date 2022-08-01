769ea46a2bbe659686877bfe5bada8f38a40b393
oukorichard <oukorichard001@gmail.com>
2022-08-01 23:12:19 +0300

    dog to D

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * init_dog - initializes a variable of type struct dog.
- * @dog: struct dog.
+ * @D: struct dog.
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: owner of the dog.
