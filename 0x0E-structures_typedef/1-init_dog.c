79c76b2dd73a11d888551435aed095fb77e9d550
oukorichard <oukorichard001@gmail.com>
2022-08-01 23:42:40 +0300

    Correcting betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,17 +6,14 @@
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: owner of the dog.
- *
  * Return: no return.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-
-  d->name = name;
-  d->age = age;
-  d->owner = owner;
-  
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
