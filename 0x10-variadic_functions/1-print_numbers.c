b508f31088a3dc2f3efbc11549dd91182657afa2
oukorichard <oukorichard001@gmail.com>
2022-08-04 18:03:18 +0300

    correcting betty style

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers.
+ * print_numbers- prints numbers.
  * @separator: string to be printed between numbers.
  * @n: number of integers passed to the function.
  *
@@ -20,7 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator && t < n - 1)
 			printf("%s", separator);
 	}
-
 	printf("\n");
 	va_end(valist);
 }
