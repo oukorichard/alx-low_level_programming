48ea7079428b74c639b983b0130c7b25f08e1f58
oukorichard <oukorichard001@gmail.com>
2022-08-04 19:44:11 +0300

    removed the main functin

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,22 +5,7 @@
 *sum_them_all- returns the sum of two values.
 *@n- the number of arguments. 
 *@return- sum of its parameters.
-*
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
-    return (0);
-}
-
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
