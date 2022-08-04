1465f5d8816ee800e959344c5aaea513a6af29a4
oukorichard <oukorichard001@gmail.com>
2022-08-04 17:25:08 +0300

    initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+/*
+*sum_them_all- returns the sum of two values.
+* n- the number of arguments. 
+* @return- sum of its parameters.
+*/
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
